b4338efa55ec596d6c1984cc8b3aa1708e4dba75
Alyreza <fanaticboy666@gmail.com>
2023-11-25 00:49:50 +0330

    add loading

--- src/screens/BasketScreen.tsx
+++ src/screens/BasketScreen.tsx
@@ -1,24 +1,36 @@
-import {StyleSheet, ScrollView, Text} from 'react-native';
+import {StyleSheet, ScrollView, Text, ActivityIndicator} from 'react-native';
 import React, {useContext, useState, useEffect} from 'react';
 import {BasketContext} from '../context/BasketContext';
 import ListItem from '../components/ListItem';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
-function BasketScreen() {
+function BasketScreen({navigation}) {
   const {items, manualUpdate} = useContext(BasketContext);
+  const [loading, setLoading] = useState(false);
   const [newItems, setItem] = useState([...items]);
   useEffect(() => {
     if (items.size > 0) {
       const intervalId = setInterval(() => {
         setItem([...manualUpdate()]);
+        setLoading(prev => !prev);
+        navigation.setOptions({
+          headerRight: () => (
+            <ActivityIndicator
+              style={styles.loading}
+              animating={loading}
+              size="small"
+              color="blue"
+            />
+          ),
+        });
       }, 5000); // 5 seconds
 
       return () => clearInterval(intervalId);
     }
-  }, [items]);
+  }, [items, loading]);
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollView}>
+      <ScrollView>
         {newItems?.map(([name, props]) => (
           <ListItem key={name} name={name} iconName="check">
             <Text style={styles.txt}>{props.status}</Text>
@@ -35,6 +47,8 @@ const styles = StyleSheet.create({
   txt: {
     paddingHorizontal: 12,
   },
-  scrollView: {},
+  loading: {
+    marginHorizontal: 8,
+  },
 });
 export default BasketScreen;
